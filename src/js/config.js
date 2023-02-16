e0ce845403f7b46a7ef1ca7b356dfa041e587e3c
Ahmed Hosny <ahmedHosny.R.M.E@gmail.com>
2023-02-16 12:05:16 +0200

    ready for deploy

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '53e560fc-fbbc-4cd5-b831-4fabf5b52cde';
+export const MODAL_CLOSE_SEC = 2.5;
